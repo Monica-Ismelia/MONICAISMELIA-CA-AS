15feded8510eaf28b7f6e75a04a58afb6670070a
angiemc98 <molinanatalia@hotmail.com>
2025-10-01 11:32:21 -0500

    Modificacion de rutas y controlador de usuario

--- Taller-2/src/usuario/usuario.controller.ts
+++ Taller-2/src/usuario/usuario.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Put, Param, Body, Delete} from '@nestjs/common';
 
 @Controller('usuario')
-export class UsuarioController {}
+export class UsuarioController {
+
+    /* <----VERBO PUT ----> */
+    @Put(':id')
+    replaceUser(@Param('id') id: number, @Body() body: {nombre: string, edad: number, email: string}) {
+
+        return { message: 'Usuario actualizado por ID', id, data: body };
+}
+    @Put()
+    replaceUserAll(@Body() body: {nombre: string, edad: number, email: string}) {
+
+        return { message: 'Usuario actualizado', data: body };
+    }
+
+    /* <----VERBO DELETE ---->*/
+    @Delete(':id')
+    deleteUser(@Param('id') id: number) {
+        return { message: 'Usuario eliminado por ID', id };
+    }
+
+    @Delete()
+    deleteUserAll(@Body() body: {ids:number[]}) {
+        return { message: 'Los usuarios que han sido eliminados son:', ids: body.ids };
+    }
+}
